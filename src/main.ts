1b26374b45b4cae7e5cf9e58e6b719d9102b8a7e
tom <935372601@qq.com>
2024-11-10 21:11:24 +0800

    项目初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+import { setup } from './setup'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+setup(app)
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vite.dev/config/
+import { visualizer } from 'rollup-plugin-visualizer'
+import veauryVitePlugins from 'veaury/vite/index.js'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    // vue(),
+    // vueJsx(),
+    splitVendorChunkPlugin(),
+    visualizer({
+      filename: './bundle-analysis.html'
+    }),
+    veauryVitePlugins({
+      type: 'vue'
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-common-lib': ['vue', 'vue-router', 'pinia'],
+          'react-common-lib': ['react', 'react-dom', '@glideapps/glide-data-grid'],
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
